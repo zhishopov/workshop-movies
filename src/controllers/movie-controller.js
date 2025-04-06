ce0841e553a23503c97d6598311583ed6811e0bf
Zhivko Shopov <zhishopov@gmail.com>
2025-04-06 23:40:23 +0100

    Take form data

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -7,6 +7,14 @@ movieController.get("/create", (req, res) => {
   res.render("create");
 });
 
+movieController.post("/create", (req, res) => {
+  console.log("POST MOVIE");
+
+  console.log(req.body);
+
+  res.end();
+});
+
 movieController.get("/:movieId/details", (req, res) => {
   const movieId = req.params.movieId;
 
--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ app.set("view engine", "hbs");
 app.set("views", "./src/views");
 
 app.use("/static", express.static("src/public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(routes);
 
