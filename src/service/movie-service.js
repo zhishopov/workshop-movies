d8101dc3cbb54e67dc7a48dcd0d3cbc77ec282f8
Zhivko Shopov <zhishopov@gmail.com>
2025-04-06 22:30:46 +0100

    Change id type

--- src/service/movie-service.js
+++ src/service/movie-service.js
@@ -3,7 +3,7 @@ import movies from "../movies.js";
 export function findMovie(movieId) {
   // TODO: if movie is missing?
 
-  const result = movies.find((movie) => movie.id == movieId);
+  const result = movies.find((movie) => movie.id === movieId);
 
   return result;
 }
