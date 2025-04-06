f150fc34b3e795b4aa6d162e690a465a0bdda579
Zhivko Shopov <zhishopov@gmail.com>
2025-04-06 19:55:20 +0100

    Fix fetching static files

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", "./src/views");
 
-app.use(express.static("src/public"));
+app.use("/static", express.static("src/public"));
 
 app.get("/", (req, res) => {
   res.render("home");
