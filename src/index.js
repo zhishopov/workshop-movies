27b39845454e43fbdbda8e3902f0b2a36baf530d
Zhivko Shopov <zhishopov@gmail.com>
2025-04-06 19:16:20 +0100

    Setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("It works");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("It works");
+});
+
+app.listen(3000, () =>
+  console.log("Server is listening on http://localhost:3000...")
+);
