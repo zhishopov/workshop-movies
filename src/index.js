5791357d9a8f5593b64c107b991922e9bc7d3bc5
Zhivko Shopov <zhishopov@gmail.com>
2025-04-06 19:58:09 +0100

    Render about page

--- src/index.js
+++ src/index.js
@@ -19,6 +19,10 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
 app.listen(3000, () =>
   console.log("Server is listening on http://localhost:3000...")
 );
