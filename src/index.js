d74c5c9dc4abb1915be74abe9e76398fe31a77b6
Zhivko Shopov <zhishopov@gmail.com>
2025-04-06 19:52:11 +0100

    Setup express static

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", "./src/views");
 
-app.use(express.static("public"));
+app.use(express.static("src/public"));
 
 app.get("/", (req, res) => {
   res.render("home");
