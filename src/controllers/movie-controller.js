11a29d3c348a7748726d38ec0e6316337ccc679b
Zhivko Shopov <zhishopov@gmail.com>
2025-04-07 07:02:15 +0100

    Extract movie partial

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -4,7 +4,8 @@ import movieService from "../service/movie-service.js";
 const movieController = Router();
 
 movieController.get("/search", (req, res) => {
-  res.render("search");
+  const movies = movieService.getAll();
+  res.render("search", { movies });
 });
 
 movieController.get("/create", (req, res) => {
