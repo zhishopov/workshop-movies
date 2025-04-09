2a76b7fe8056b41de062cc2d9a433a29fc1402ad
Zhivko Shopov <zhishopov@gmail.com>
2025-04-09 12:48:32 +0100

    Add casts filter

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -36,7 +36,7 @@ movieController.get("/:movieId/details", async (req, res) => {
 movieController.get("/:movieId/attach-cast", async (req, res) => {
   const movieId = req.params.movieId;
   const movie = await movieService.getOne(movieId);
-  const casts = await castService.getAll();
+  const casts = await castService.getAll({ exclude: movie.casts });
 
   res.render("movie/attach-cast", { movie, casts });
 });
--- src/service/cast-service.js
+++ src/service/cast-service.js
@@ -1,8 +1,14 @@
 import Cast from "../models/Cast.js";
 
 export default {
-  getAll() {
-    return Cast.find({});
+  getAll(filter = {}) {
+    let query = Cast.find({});
+
+    if (filter.exclude) {
+      query = query.find({ _id: { $nin: filter.exclude } });
+    }
+
+    return query;
   },
   create(castData) {
     return Cast.create(castData);
